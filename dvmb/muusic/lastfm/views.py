202342ed29666be77f5eb490b34039127f18f812
dnf-08 <rohannair0308@gmail.com>
2026-09-25 14:24:26 +0530

    added views for the lastfm methods

--- dvmb/muusic/lastfm/views.py
+++ dvmb/muusic/lastfm/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
-
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from .api import topartists,toptracks,artistsearch,albumsearch,tracksearch
 # Create your views here.
+def topartistsv(request):
+    country = request.GET.get('country', 'United States')
+    artistdic = topartists(country)
+    info = {
+    'artists': artistdic,
+    'country': country,
+    }
+    return render(request, 'lastfm/topartist.html', info)
+
+def toptracksv(request):
+    country = request.GET.get('country', 'United States')
+    trackdic = toptracks(country)
+    info = {
+    'tracks': trackdic,
+    'country': country,
+    }
+    return render(request, 'lastfm/toptrack.html', info)
+
+
+def artistsearchv(request):
+    artist = request.GET.get('artist', 'Radiohead')
+    artistdic = artistsearch(artist)
+    info = {
+    'artists': artistdic,
+    'input': artist,
+    }
+    return render(request, 'lastfm/artistsearch.html', info)
+
+
+def albumsearchv(request):
+    album = request.GET.get('album', 'Nevermind')
+    albumdic = albumsearch(album)
+    info = {
+    'albums': albumdic,
+    'input': album,
+    }
+    return render(request, 'lastfm/albumsearch.html', info)
+
+def tracksearchv(request):
+    track = request.GET.get('track', 'Someday')
+    trackdic = tracksearch(track)
+    info = {
+    'tracks': trackdic,
+    'input': track,
+    }
+    return render(request, 'lastfm/tracksearch.html', info)
+
